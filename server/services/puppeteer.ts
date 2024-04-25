d4e6d233abb6dda9b3fea89fee82299b2d54fae4
anujnema5 <anujnema63@gmail.com>
2024-04-25 14:44:59 +0530

    minor changes

--- server/services/puppeteer.ts
+++ server/services/puppeteer.ts
@@ -4,9 +4,16 @@ import { setTimeout } from "node:timers/promises";
 const url = 'https://youtube.com/';
 
 export const puppeteerService = async (value: string) => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto(url);
+
+    /** The thing I was trying to implement for automation on the same instance */
+
+    // const browserWSEndpoint = 'https://www.youtube.com/'; 
+    // const browser = await puppeteer.connect({ browserWSEndpoint });
+    // const pages = await browser.pages();
+    // const page = pages[0]; 
 
     await page.waitForSelector('input#search');
     await page.waitForSelector('button#search-icon-legacy');
@@ -14,13 +21,14 @@ export const puppeteerService = async (value: string) => {
     await page.type('input#search', value);
     await page.click('button#search-icon-legacy');
 
-    await setTimeout(2000)
+    await setTimeout(3000)
 
     const videoTitles = await page.evaluate(() => {
         const results = [] as Object[];
         const items = document.querySelectorAll('h3 a.yt-simple-endpoint.style-scope');
 
         items.forEach((element: any) => {
+            console.log('triggering here')
             results.push({ title: element.textContent.trim() });
         });
 
